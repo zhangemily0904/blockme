aa3108fc32977e55a1d89ef6c4e9e2d23d1d32e5
bchou123 <briancho@andrew.cmu.edu>
2022-10-23 21:17:14 -0400

    initial commit, linked project to firestore

---
+++ BlockMe/Views/BlockMeApp.swift
@@ -0,0 +1,30 @@
+//
+//  BlockMeApp.swift
+//  BlockMe
+//
+//  Created by Brian Chou on 10/23/22.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct BlockMeApp: App {
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+  
+  var body: some Scene {
+      WindowGroup {
+          ContentView()
+      }
+  }
+}
