6d6c0fc8791660f78b9973bc6a789edef14f8a47
Wen Shan Jiang <wenshanj@andrew.cmu.edu>
2022-11-08 13:58:01 -0500

    resolve merge conflict

---
+++ BlockMe/ViewModels/VenmoFormatter.swift
@@ -0,0 +1,8 @@
+//
+//  VenmoFormatter.swift
+//  BlockMe
+//
+//  Created by Wen Shan Jiang on 11/8/22.
+//
+
+import Foundation
---
+++ BlockMe/Views/WelcomeView.swift
@@ -0,0 +1,8 @@
+//
+//  WelcomeView.swift
+//  BlockMe
+//
+//  Created by Wen Shan Jiang on 11/8/22.
+//
+
+import Foundation
