e39ed6ab9ee9f25d6f5f0d18b618e92015532375
Wen Shan Jiang <wenshanj@andrew.cmu.edu>
2022-11-08 00:04:11 -0500

    reformat fname, lname, venmo, phone

--- BlockMe/Views/UIComponents.swift
+++ BlockMe/Views/UIComponents.swift
@@ -32,6 +32,20 @@ struct InputField: TextFieldStyle {
       .disableAutocorrection(true)
   }
 }
+
+struct CapitalizationInputField: TextFieldStyle {
+  func _body(configuration: TextField<Self._Label>) -> some View {
+    configuration
+      .padding()
+      .frame(width: 352, height: 64)
+      .overlay {
+        RoundedRectangle(cornerRadius: 16, style: .continuous)
+          .stroke(Color.black, lineWidth: 2)
+      }
+      .autocapitalization(.words)
+      .disableAutocorrection(true)
+  }
+}
   
 struct SmallWhiteButton: ButtonStyle {
   func makeBody(configuration: Configuration) -> some View {
---
+++ BlockMe/Views/VenmoFormatter.swift
@@ -0,0 +1,43 @@
+//
+//  VenmoFormatter.swift
+//  BlockMe
+//
+//  Created by Wen Shan Jiang on 11/8/22.
+//
+
+import Foundation
+
+
+class VenmoFormatter: Formatter {
+  let pattern = "^[a-zA-Z0-9_-]*$"
+  
+  // venmo usernames should only contain letters, numbers, hyphen, and underscore. It must also be 5-30 characters long
+  private func isValidVenmo(_ value: String) -> Bool {
+    return (value.range(of: pattern, options: .regularExpression) != nil) && value.count > 4 && value.count < 31
+  }
+
+  override func string(for obj: Any?) -> String? {
+      guard let string = obj as? String,
+            isValidVenmo(string) else { return nil }
+
+      return "@\(string)"
+  }
+  
+  override func getObjectValue(
+      _ obj: AutoreleasingUnsafeMutablePointer<AnyObject?>?,
+      for string: String,
+      errorDescription error: AutoreleasingUnsafeMutablePointer<NSString?>?
+  ) -> Bool {
+   
+      let venmoValue: String
+      if string.contains("@") {
+        venmoValue = String(string.dropFirst())
+      } else {
+        venmoValue = string
+      }
+      
+      obj?.pointee = venmoValue as AnyObject
+      return true
+  }
+  
+}
