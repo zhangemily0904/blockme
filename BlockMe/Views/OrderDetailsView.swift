1ead5177c38691b5a2d9fdc41506c6bdc1171ddc
Emily Zhang <zilu.zhang0904@gmail.com>
2022-11-09 00:29:56 -0500

    added orders and orders detail view

---
+++ BlockMe/Views/OrderDetailsView.swift
@@ -0,0 +1,20 @@
+//
+//  OrderDetailsView.swift
+//  BlockMe
+//
+//  Created by Emily Zhang on 11/8/22.
+//
+
+import SwiftUI
+
+struct OrderDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OrderDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderDetailsView()
+    }
+}
