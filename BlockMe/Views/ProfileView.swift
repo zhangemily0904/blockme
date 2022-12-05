9a9731092658a39f4efedbbe0d0c79adf6184e8e
Wen Shan Jiang <wenshanj@andrew.cmu.edu>
2022-12-05 14:06:04 -0500

    style profile view

--- BlockMe/Views/ProfileView.swift
+++ BlockMe/Views/ProfileView.swift
@@ -16,28 +16,59 @@ struct ProfileView: View {
     ZStack {
       Color("BlockMe Background").ignoresSafeArea()
       VStack {
-        Text("Profile").font(.title)
-        
-        if let userViewModel = appViewModel.userViewModel {
-          if let profileImage = userViewModel.profileImage {
-            Image(uiImage: profileImage)
-              .resizable()
-              .scaledToFit()
-              .clipShape(Circle())
+        ZStack {
+          Image("profile").resizable().scaledToFit().offset(y: -150)
+          if let userViewModel = appViewModel.userViewModel {
+            if let profileImage = userViewModel.profileImage {
+              Image(uiImage: profileImage)
+                .resizable()
+                .frame(width: 160, height: 160)
+                .clipShape(Circle())
+                .offset(y: -50)
+            }
           }
+        }
+        if let userViewModel = appViewModel.userViewModel {
           if let user = userViewModel.user {
-            Text("\(user.firstName) \(user.lastName)").font(.headline)
+            Text("\(user.firstName) \(user.lastName)").font(.medMed)
+              .offset(y: -60)
+          }
+          HStack {
+            VStack {
+              NavigationLink("Manage Account", destination: EditProfileView())
+              Text("Update your information").font(.medTiny).foregroundColor(.gray).offset(x: 5)
+            }.frame(width: 300, alignment: .leading)
+              .padding(.leading, 15)
+            Image("next")
+              .resizable()
+              .frame(width: 20, height: 20, alignment: .trailing)
+              .padding(.trailing, 15)
+          }
+            .frame(width: 353, height: 75)
+            .background(Color("BlockMe Yellow").clipShape(RoundedRectangle(cornerRadius:15)))
+            .font(.medSmall)
+            .foregroundColor(.black)
+            .offset(y: -20)
+        }
+        
+        HStack {
+          Button(action: {
+//            appViewModel.signOut()
+            print("click")
+          }) {
+            Text("Sign Out").font(.medSmall).frame(width: 295, alignment: .leading)
+              .padding(.leading, 70)
+            Image("next")
+              .resizable()
+              .frame(width: 20, height: 20, alignment: .trailing)
+              .padding(.trailing, 70)
           }
           
-          Spacer()
-          NavigationLink("Manage Account", destination: EditProfileView())
         }
-        
-        Button(action: {
-          appViewModel.signOut()
-        }) {
-          Text("Sign Out").bold()
-        }
+        .frame(width: 353, height: 75)
+        .background(Color("BlockMe Yellow").clipShape(RoundedRectangle(cornerRadius:15)))
+        .foregroundColor(.black)
+        .offset(y: -15)
       }
     }
   }
