c4462c441f238ba9146595139ed767b6f40d0ff8
Emily Zhang <zilu.zhang0904@gmail.com>
2022-12-08 22:41:48 -0500

    idk whats going on

--- BlockMeTests/BlockMeTests.swift
+++ BlockMeTests/BlockMeTests.swift
@@ -7,29 +7,34 @@
 
 import XCTest
 
+//@testable import BlockMe
+
 final class BlockMeTests: XCTestCase {
+  // Start with a basic setup method
+  let expired: TimeInterval = 1.5
+  var expectation: XCTestExpectation!
+  let listingRepository = ListingRepository()
+  
+  override func setUpWithError() throws {
+    expectation = expectation(description: "Server responds in reasonable time")
+  }
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+  override func tearDownWithError() throws {
+      // Put teardown code here. This method is called after the invocation of each test method in the class.
+  }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+  func testExample() throws {
+    XCTAssertTrue(listingRepository.listings.count > 0)
+    self.expectation.fulfill()
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+    waitForExpectations(timeout: expired)
+  }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+  func testPerformanceExample() throws {
+    // This is an example of a performance test case.
+    measure {
+        // Put the code you want to measure the time of here.
     }
+  }
 
 }
